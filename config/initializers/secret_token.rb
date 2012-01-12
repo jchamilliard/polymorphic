ec1fe379e1f8730a30dbd653aa9dc569b88fd0f5
Jeremy Chamilliard <jchamilliard@yammer-inc.com>
2012-01-11 22:01:38 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Polymorphic::Application.config.secret_token = '47651a15043cf73b5551f11e09af0f1172084985e276c4775d9894a1060a3241f485c2290d0846be55dcb4a9556056f6574fdc119334011d815db5af6565921a'
